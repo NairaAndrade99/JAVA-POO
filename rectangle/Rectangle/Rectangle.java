bd4b075d554a88f1e9605786b42118ed4d6c32b1
Naira da Silva <naira.andrade35@gmail.com>
2024-10-09 09:25:30 -0300

    resctangle 1.2

--- rectangle/Rectangle/Rectangle.java
+++ rectangle/Rectangle/Rectangle.java
@@ -8,7 +8,7 @@ public class Rectangle {
         return width * height;
     }
     public double perimeter(){
-        return (width + height ) / 2;
+        return (width + height ) * 2;
     }
     public double diagonal (){
         return Math.sqrt(Math.pow(width, 2 ) + Math.pow(height, 2));
