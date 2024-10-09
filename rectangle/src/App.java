a9a351ee761fdea99dd6e83cdfb40d530d51af54
Naira da Silva <naira.andrade35@gmail.com>
2024-10-08 21:24:56 -0300

    Rectangle - 1.0

---
+++ rectangle/src/App.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import entities.Rectangle.*;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner sc = new Scanner (System.in);
+
+        Rectangle rectangle = new Rectangle();
+
+        System.out.println("Enter rectangle with and height: ");
+        rectangle.width = sc.nextDouble();
+        rectangle.height = sc.nextDouble();
+
+         System.out.println(rectangle);
+    }
+}
