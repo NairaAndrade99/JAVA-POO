0a7608ecb218b4faa4b2b152cd5ffab8d7692ed3
Naira da Silva <naira.andrade35@gmail.com>
2024-10-10 01:05:00 -0300

    employee 1.0

---
+++ employee/employee/Employee.java
@@ -0,0 +1,22 @@
+package employee.employee;
+
+public class Employee {
+    public String name;
+    public double grossSalary, tax;
+
+public double netSalary (){
+    return grossSalary -= tax;
+
+}
+public void increaseSalary(double percentage){
+     grossSalary += percentage / 100 * netSalary();
+     System.out.println(grossSalary);
+}
+public String toString(){
+     return name +
+            String.format(", $ %.2f ", netSalary())
+            + name +
+            String.format("$ ", increaseSalary());
+}
+
+}
---
+++ employee/src/App.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import employee.employee.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+         
+        Scanner sc = new Scanner (System.in);
+        
+        Employee employee = new Employee ();
+
+        System.out.println("Name: ");
+        employee.name = sc.nextLine();
+
+        System.out.println("Gross salary: ");
+        employee.grossSalary = sc.nextDouble();
+
+        System.out.println("Tax: ");
+        employee.tax = sc.nextDouble();
+
+        System.out.println("Employee: ", employee);
+
+        System.out.println("Wich percentage to increase salary ? ");
+        double perce = sc.nextDouble();
+        employee.increaseSalary(perce);
+
+
+
+        
+    }
+}
